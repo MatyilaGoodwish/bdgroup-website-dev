7ccb0a8737c07d1555e49f1e3c22344cde023b44
Goodwish Matyila <goodwish@axxess.co.za>
2019-05-31 20:20:15 +0200

    update

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<?php require_once("inc/header.php") ?>
+
+
+      <div class="container m-2">
+            <div class="row">
+                  <div class="col-md-6 p-2">
+                        <h2 class="display-2 text-muted">
+                              Website Notice.
+                        </h2>
+                        <p class="lead">
+                              We are currently down for maintenance on our website should you need anything kindly contact us directory on <br/>087 057-1581
+                        </p>
+                  </div>
+                  <div class="col-md-6 p-2">
+                        <img src="images/home-right.png" class="w-100 h-100"/>
+                  </div>
+            </div>
+      </div>
+      
+
+<?php require_once("inc/footer.php")  ?>
